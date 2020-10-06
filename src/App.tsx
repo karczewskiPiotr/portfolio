4e45c68cb5e0ec7d83822aabc0dfecfb68a9743f
Piotrek Karczewski <p.karczewsk@gmail.com>
2020-10-06 17:30:06 +0200

    Create typography classes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,22 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <div className='App'>
-    Hello World!
-  </div>;
+  return (
+    <div className='App'>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <p>
+        A paragraph (from the Greek paragraphos, “to write beside” or “written
+        beside”) is a self-contained unit of a discourse in writing dealing with
+        a particular point or idea. A paragraph consists of one or more
+        sentences. Though not required by the syntax of any language, paragraphs
+        are usually an expected part of formal writing, used to organize longer
+        prose.
+      </p>
+    </div>
+  );
 };
 
 export default App;
