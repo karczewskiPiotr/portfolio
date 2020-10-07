51bd02442aac6995803d408594ab304fa5d616e9
Piotrek Karczewski <p.karczewsk@gmail.com>
2020-10-07 22:55:23 +0200

    Animate navbar

--- src/components/dropdown.tsx
+++ src/components/dropdown.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import '../styles/dropdown.css';
 import { useTranslation } from 'react-i18next';
+import { Transition } from 'react-spring/renderprops';
 
-const Dropdown = () => {
+type DropdownProps = {
+  isOpen: boolean;
+};
+
+const Dropdown = ({ isOpen }: DropdownProps) => {
   const { t } = useTranslation();
 
   return (
-    <div className='dropdown'>
-      <ul>
-        <li className='margin-bottom-sm'>{t('menu.work')}</li>
-        <li className='margin-bottom-sm'>{t('menu.skills')}</li>
-        <li className='margin-bottom-sm'>{t('menu.about')}</li>
-        <li className='margin-bottom-sm'>{t('menu.cv')}</li>
-      </ul>
-    </div>
+    <Transition
+      items={isOpen}
+      from={{ height: 0, overflow: 'hidden' }}
+      enter={{ height: "auto", overflow: 'hidden' }}
+      leave={{ height: 0, overflow: 'hidden' }}
+    >
+      {(isOpen) =>
+        isOpen &&
+        ((props) => (
+          <div
+            style={props}
+            className='dropdown'
+          >
+            <ul>
+              <li className='margin-bottom-sm'>{t('menu.work')}</li>
+              <li className='margin-bottom-sm'>{t('menu.skills')}</li>
+              <li className='margin-bottom-sm'>{t('menu.about')}</li>
+              <li className='margin-bottom-sm'>{t('menu.cv')}</li>
+            </ul>
+          </div>
+        ))
+      }
+    </Transition>
   );
 };
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,10 @@
 import React, { useState, Suspense } from 'react';
 import '../styles/navbar.css';
 import Logo from '../images/logo.svg';
+import Back from '../images/back.svg';
 import Menu from '../images/hamburger-menu.svg';
 import Dropdown from './dropdown';
+import { Transition } from 'react-spring/renderprops';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,19 +16,41 @@ const Navbar = () => {
         alt='Piotr Karczewski Logo'
         className='logo margin-left-xxs'
       />
-      <img
-        src={Menu}
-        alt='Hamburger menu'
-        className='menu-icon margin-right-sm'
-        onClick={() => {
-          setIsOpen((prev) => !prev);
-        }}
-      />
-      {isOpen && (
-        <Suspense fallback={<div>Loding</div>}>
-          <Dropdown />
-        </Suspense>
-      )}
+      <Transition
+        items={isOpen}
+        from={{ opacity: 0, transform: 'rotate(-270deg)' }}
+        enter={{ opacity: 1, transform: 'rotate(0deg)' }}
+        leave={{ opacity: 0, transform: 'rotate(270deg)' }}
+      >
+        {(isOpen) =>
+          !isOpen
+            ? (props) => (
+                <img
+                  style={props}
+                  src={Menu}
+                  alt='Hamburger menu'
+                  className='menu-icon margin-right-sm'
+                  onClick={() => {
+                    setIsOpen((prev) => !prev);
+                  }}
+                />
+              )
+            : (props) => (
+                <img
+                  style={props}
+                  src={Back}
+                  alt='Hamburger menu'
+                  className='menu-icon margin-right-sm'
+                  onClick={() => {
+                    setIsOpen((prev) => !prev);
+                  }}
+                />
+              )
+        }
+      </Transition>
+      <Suspense fallback={<div>Loding</div>}>
+        <Dropdown isOpen={isOpen} />
+      </Suspense>
     </nav>
   );
 };
