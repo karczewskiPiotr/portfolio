7e7f0f5ef113b00df0e489da5cadaa772b778477
Piotrek Karczewski <p.karczewsk@gmail.com>
2020-10-12 18:18:12 +0200

    Animate carousel

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'react-i18next';
 import Quotation from '../images/quotations.svg';
 import ArrowLeft from '../images/arrow-left.svg';
 import ArrowRight from '../images/arrow-right.svg';
+import { Transition } from 'react-spring/renderprops';
 
 const Testimonials: React.FC = () => {
   const { t } = useTranslation();
@@ -14,19 +15,16 @@ const Testimonials: React.FC = () => {
   const testimonialsRef = useFirestore().collection('locales/en/testimonials');
   const testimonialsData = useFirestoreCollectionData(testimonialsRef);
 
-  const getTestimonialCard = (index: number): JSX.Element => {
+  const getTestimonialCard = (index: number, props: object): JSX.Element => {
     const testimonial = testimonialsData[index] as TestimonialType;
 
     return (
-      <div className='testimonial-card'>
+      <div className='testimonial-card' style={props}>
         <img className='quotation-image' src={Quotation} alt='Quotation mark' />
         <p className='testimonial-content'>{testimonial.content}</p>
         <div className='testimonial-author'>
           <div className='author-image'>
-            <img
-              src={testimonial.picture}
-              alt={`${testimonial.author}`}
-            />
+            <img src={testimonial.picture} alt={`${testimonial.author}`} />
           </div>
           <div className='margin-right-xs'>
             <p className='author'>{testimonial.author}</p>
@@ -61,7 +59,23 @@ const Testimonials: React.FC = () => {
     <section className='testimonials-wrapper margin-horizontal-sm'>
       <h3 className='margin-bottom-lg'>{t('testimonials')}</h3>
       <div className='carousel'>
-        {getTestimonialCard(focus)}
+        <Transition
+          items={focus}
+          from={{
+            opacity: 0,
+            transform: 'scale(0.6)',
+          }}
+          enter={{
+            opacity: 1,
+            transform: 'scale(1)',
+          }}
+          leave={{
+            opacity: 0,
+            transform: 'scale(0.6)',
+          }}
+        >
+          {(focus) => (props) => getTestimonialCard(focus, props)}
+        </Transition>
         <img
           className='arrow-left'
           onClick={focusOnPrevious}
