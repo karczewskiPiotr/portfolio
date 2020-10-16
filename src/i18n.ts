02bd68117228a6ae5fcce11f1829b92794ab878a
Piotrek Karczewski <p.karczewsk@gmail.com>
2020-10-16 22:56:06 +0200

    Change internationalization handling

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,16 +1,24 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+import translationEN from './locales/en/translation.json';
+import translationPL from './locales/pl/translation.json';
 
-i18n
-  .use(Backend)
-  .use(initReactI18next)
-  .init({
-    lng: 'en',
-    debug: true,
-    interpolation: {
-      escapeValue: false,
-    },
-  });
+const resources = {
+  en: {
+    translation: translationEN,
+  },
+  nl: {
+    translation: translationPL,
+  },
+};
+
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  debug: true,
+  resources,
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
