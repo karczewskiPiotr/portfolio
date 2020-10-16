60ecc954a195994d078c08029619859534faba45
Piotrek Karczewski <p.karczewsk@gmail.com>
2020-10-16 20:48:32 +0200

    Remove enter animation breaking sticky navigation

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,10 @@ import Skills from './components/skills';
 import Collaborations from './components/collaborations';
 import Projects from './components/projects';
 import Testimonials from './components/testimonials';
-import { animated, useSpring } from 'react-spring';
 
 const App: React.FC = () => {
-  const props = useSpring({
-    to: { opacity: 1, transform: 'translateX(0) scale(1)' },
-    from: { opacity: 0, transform: 'translateX(-5px) scale(0.8)' },
-  });
-
   return (
-    <animated.div style={props} className='app'>
+    <div className='app'>
       <Navbar />
       <Landing />
       <div className='pattern-background'>
@@ -25,7 +19,7 @@ const App: React.FC = () => {
       </div>
       <Testimonials />
       <Collaborations />
-    </animated.div>
+    </div>
   );
 };
 
