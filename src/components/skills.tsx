1168d24512d5b80c31d52d07aa4a726cf03cb0ae
Piotrek Karczewski <p.karczewsk@gmail.com>
2020-10-19 00:09:27 +0200

    Move technologies to a separate file

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,35 +1,10 @@
 import React from 'react';
 import '../styles/skills.css';
 import { useTranslation } from 'react-i18next';
-
-type Technologies = {
-  main: string[];
-  complementary: string[],
-  rare: string[]
-}
+import { technologies } from '../constants/technologies';
 
 const Skills: React.FC = () => {
   const { t } = useTranslation();
-
-  const technologies: Technologies = {
-    main: [
-      'Ruby',
-      'Rails',
-      'PostgreSQL',
-      'RSpec',
-      'TypeScript',
-      'JavaScript',
-      'React',
-      'Redux',
-      'HTML5',
-      'CSS',
-      'Sass',
-      'Webpack',
-      'Figma',
-    ],
-    complementary: ['Redis', 'Sidekiq', 'Firebase', 'HAML'],
-    rare: ['Java', 'C++', 'C#'],
-  };
 
   const getTechnologies = (technologies: string[]): JSX.Element[] => {
     return technologies.map((technology) => {
@@ -38,27 +13,37 @@ const Skills: React.FC = () => {
         .replace('#', '')}-logo.svg`);
 
       return (
-        <div className='technology' key={technology}>
+        <article className='technology' key={technology}>
           <img src={logo} alt={`${technology} icon`} />
           <p>{technology}</p>
-        </div>
+        </article>
       );
     });
   };
 
   return (
-    <section id='skills' className='skills-wrapper margin-horizontal-sm'>
-      <div className='skills'>
-        <h4>{t('skills.part1')}</h4>
-        <div className='technologies'>{getTechnologies(technologies.main)}</div>
-        <h4 className='margin-top-sm'>{t('skills.part2')}</h4>
-        <div className='technologies'>
-          {getTechnologies(technologies.complementary)}
-        </div>
-        <h4 className='margin-top-sm'>{t('skills.part3')}</h4>
-        <div className='technologies'>{getTechnologies(technologies.rare)}</div>
-      </div>
-    </section>
+    <div id='skills' className='skills-wrapper'>
+      <section className='skills'>
+        <article>
+          <h4>{t('skills.part1')}</h4>
+          <div className='technologies'>
+            {getTechnologies(technologies.main)}
+          </div>
+        </article>
+        <article>
+          <h4 className='margin-top-sm'>{t('skills.part2')}</h4>
+          <div className='technologies'>
+            {getTechnologies(technologies.complementary)}
+          </div>
+        </article>
+        <article>
+          <h4 className='margin-top-sm'>{t('skills.part3')}</h4>
+          <div className='technologies'>
+            {getTechnologies(technologies.rare)}
+          </div>
+        </article>
+      </section>
+    </div>
   );
 };
 
---
+++ src/constants/technologies.ts
@@ -0,0 +1,25 @@
+type Technologies = {
+  main: string[];
+  complementary: string[],
+  rare: string[]
+}
+
+export const  technologies: Technologies = {
+  main: [
+    'Ruby',
+    'Rails',
+    'PostgreSQL',
+    'RSpec',
+    'TypeScript',
+    'JavaScript',
+    'React',
+    'Redux',
+    'HTML5',
+    'CSS',
+    'Sass',
+    'Webpack',
+    'Figma',
+  ],
+  complementary: ['Redis', 'Sidekiq', 'Firebase', 'HAML'],
+  rare: ['Java', 'C++', 'C#'],
+};
